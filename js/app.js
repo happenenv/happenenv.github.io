4c0bbf8c340c79ca85c940c64cd71b524cc93955
mauricio_junior <jmauriciolm38@gmail.com>
2017-12-09 16:37:31 -0200

    relogio e acoes funcionando

--- js/app.js
+++ js/app.js
@@ -2,21 +2,27 @@ var happenApp = angular.module('happenApp', []);
 
 happenApp.controller('RingueController', function RingueController($scope) {
 
-	var segundo = 0+"0";
+	var segundo = 0;
 	var minuto = 1;
-	$scope.tempo = "00:00";
 	var intervalo;
+	$scope.tempo = "00:00";
+	$scope.tempoInicial;
+	$scope.pausa = 0;
+	$scope.inciado = 0;
 
 	function relogio () {
 		console.log('tic tac');
 		if(segundo > 0){
 			segundo--;
+			if(segundo < 10){
+				segundo = "0"+segundo;
+			}
 		}
 		else if(segundo == 0){
 			minuto--;
 			if(minuto < 0){
 				segundo = "00";
-				end();
+				parar();
 			}else{
 				segundo = 59;
 			}
@@ -26,12 +32,27 @@ happenApp.controller('RingueController', function RingueController($scope) {
 		console.log($scope.tempo);
 	}
 
-	$scope.start = function () {
+	$scope.comecar = function () {
+		minuto = $scope.tempoInicial;
+		$scope.inciado = 1;
 		intervalo = window.setInterval(relogio, 1000);
-		console.log('foi');
 	}
 
-	$scope.end = function () {
-		window.clearInterval();
+	$scope.parar = function () {
+		$scope.pausa = 1;
+		window.clearInterval(intervalo);
+	}
+
+	$scope.retomar = function () {
+		$scope.pausa = 0;
+		intervalo = window.setInterval(relogio, 1000);
+	}
+
+	$scope.reiniciar = function () {
+		$scope.pausa = 0;
+		$scope.inciado = 0;
+		$scope.tempo = "00:00";
+		minuto = segundo = 0;
+		window.clearInterval(intervalo);
 	}
 });
